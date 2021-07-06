ad76258429f527ca38883ac210c8ddde35f1204d
Uğur Selim Özen <ugur_ozen58@hotmail.com>
2021-07-06 17:59:54 +0300

    Update HW3.sql

--- SQL Course/HW3.sql
+++ SQL Course/HW3.sql
@@ -5,6 +5,10 @@
 --List the country names from the country column of the country table, consisting of at least 6 characters and ending with the 'n' character.
 
 -- QUERY 3
+--Containing at least 4 'T' characters, regardless of upper or lower case, from the movie names in the title column of the movie table
+--List the movie titles.
+
+-- QUERY 4
 --From the data in all the columns in the movie table, sort the data that starts with the title 'C' character, has a length greater than 90 
 --and a rental_rate of 2.99.
 
@@ -15,6 +19,9 @@ WHERE country LIKE 'A%a' ;
 
 SELECT country FROM country
 WHERE country LIKE '_____%n' ;
+
+SELECT title FROM film
+WHERE title ILIKE '%T%T%T%T%' ;
 
 SELECT * FROM film
 WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99 ;
