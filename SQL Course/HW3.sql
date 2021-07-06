c1f043dc8bfa937eb92998e15d8382d144e2ac57
Uğur Selim Özen <ugur_ozen58@hotmail.com>
2021-07-06 16:02:07 +0300

    Create HW3.sql

---
+++ SQL Course/HW3.sql
@@ -0,0 +1,22 @@
+-- QUERY 1 
+--List the country names from the country column of the country table, starting with the 'A' character and ending with the 'a' character.
+
+-- QUERY 2
+--List the country names from the country column of the country table, consisting of at least 6 characters and ending with the 'n' character.
+
+-- QUERY 3
+--From the data in all the columns in the movie table, sort the data that starts with the title 'C' character, has a length greater than 90 
+--and a rental_rate of 2.99.
+
+
+
+SELECT country FROM country
+WHERE country LIKE 'A%a' ;
+
+SELECT country FROM country
+WHERE country LIKE '_____%n' ;
+
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99 ;
+
+
